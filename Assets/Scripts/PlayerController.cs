cc29c02124d986ad5b8203ffb3d5cbd0fb6021fa
mgril <112563882+mgril@users.noreply.github.com>
2023-10-14 12:35:17 +0200

    Animation/Package Cinemachine/camera/keyboard
    The camera keep look on player , the animation run and idle work correctly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [RequireComponent(typeof(CharacterController))]
+
+    public class PlayerController : MonoBehaviour
     {
+
+        private CharacterController _controller;
+        private Transform _animatorTransform;
+        [SerializeField] private Animator _animator;
+
+        #region Variables: Movement
+        private const float _MOVE_SPEED = 5f;
+        private Vector2 _move;
+        private bool _running;
+        #endregion
         
+        #region Variables: Inputs
+        private DefaultInputActions _inputActions;
+        private InputAction _moveAction;
+        #endregion
+
+        #region Variables: Animation
+        private int _animRunningParamHash;
+        #endregion
+
+        void Awake()
+        {
+            _inputActions = new DefaultInputActions();
+            _controller = GetComponent<CharacterController>();
+
+            _running = false;
+            _animRunningParamHash = Animator.StringToHash("Running");
+
+            _animatorTransform = _animator.transform;
+        }
+        private void OnEnable()
+        {
+            _moveAction = _inputActions.Player.Move;
+            _moveAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _moveAction.Disable();
+        }
+
+        void Update()
+        {
+            _move = _moveAction.ReadValue<Vector2>();
+            if (_move.sqrMagnitude > 0.01f)
+            {
+                if (!_running)
+                {
+                    _running = true;
+                    _animator.SetBool(_animRunningParamHash, true);
+                }
+
+                Vector3 v = new Vector3(_move.x, 0f, _move.y);
+                _animatorTransform.rotation =Quaternion.LookRotation(-v, Vector3.up);
+                _controller.Move(
+                    v *
+                    Time.deltaTime *
+                    _MOVE_SPEED);
+            }
+            else if (_running)
+            {
+                _running = false;
+                _animator.SetBool(_animRunningParamHash, false);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
